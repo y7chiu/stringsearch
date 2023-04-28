e78d744942f371bebf333c636600df77cfbec304
Joe Politz <joe@cs.brown.edu>
2023-04-28 14:16:54 -0700

    little interface cleanups

--- StringServer.java
+++ StringServer.java
@@ -55,6 +55,10 @@ class StringServer {
       System.out.println("Missing port number! Try any number between 1024 to 49151");
       return;
     }
+    if(args.length == 1){
+      System.out.println("Missing file path! Give a path to a text file as the second argument.");
+      return;
+    }
 
     int port = Integer.parseInt(args[0]);
 
